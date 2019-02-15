a12ece107eff117c8c90c52fe175904d445d9ef7
Ricardo <ricardo_mpsp@hotmail.com>
2019-02-15 14:04:21 -0600

    Codigo de fragmento de Guia añadido

--- app/src/main/java/com/example/ricar/alertify/InicioActiv.java
+++ app/src/main/java/com/example/ricar/alertify/InicioActiv.java
@@ -3,6 +3,8 @@ package com.example.ricar.alertify;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -16,15 +18,26 @@ public class InicioActiv extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fm=getSupportFragmentManager();
+            FragmentTransaction transaction=fm.beginTransaction();
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                //REEMPLAZAR CODIGO CON EL DEL FRAGMENTO PROPIO
+                case R.id.navigation_inicio:
+                    mTextMessage.setText(R.string.title_inicio);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_mapa:
+                    mTextMessage.setText(R.string.title_mapa);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_reportes:
+                    mTextMessage.setText(R.string.title_reportes);
+                    return true;
+                case R.id.navigation_guia:
+                    mTextMessage.setText(R.string.title_guia);
+                    GuiaFrag fragGuia=new GuiaFrag();
+                    transaction.replace(R.id.contFrag,fragGuia);
+                    transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
                     return true;
             }
             return false;
@@ -36,7 +49,7 @@ public class InicioActiv extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        mTextMessage = (TextView) findViewById(R.id.tvTitle);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
